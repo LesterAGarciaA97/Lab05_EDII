0bdd15216852831fae5a88739c2d8681b7deb264
Lester Andrés García Aquino <lagarcia2015url@gmail.com>
2020-11-06 15:35:35 -0600

    Se quitador Using innecesarios

--- Solución/Lab05_EDII/Lab05_EDII/Controllers/ApiController.cs
+++ Solución/Lab05_EDII/Lab05_EDII/Controllers/ApiController.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Lab05_EDII.Models;
 using Microsoft.AspNetCore.Http;
--- Solución/Lab05_EDII/Lab05_EDII/Models/Key.cs
+++ Solución/Lab05_EDII/Lab05_EDII/Models/Key.cs
@@ -1,10 +1,4 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Lab05_EDII.Models
+﻿namespace Lab05_EDII.Models
 {
     public class Key : IKey
     {
